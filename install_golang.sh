a8776152bbdf626cb2d358a0fb51f198f447c4b1
gLAd <31668505+mr0wnage@users.noreply.github.com>
2021-01-12 20:56:29 +0300

    Update install_golang.sh

--- install_golang.sh
+++ install_golang.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-GO_VERSION="1.14.2"
+GO_VERSION=${GO_VERSION:=1.15.2}
 
 cd /tmp/ \
-&& wget https://dl.google.com/go/go$GO_VERSION.linux-amd64.tar.gz \
-&& sudo tar -C /usr/local -xzf go*.tar.gz \
-&& sudo rm go$GO_VERSION*.gz
+&& wget -q https://dl.google.com/go/go$GO_VERSION.linux-amd64.tar.gz \
+&& rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go*.tar.gz \
+&& rm go$GO_VERSION*.gz
 
 echo "Creating default Go folders"
 mkdir -p $HOME/go/bin
@@ -14,7 +14,7 @@ if [[ -z "${GOROOT}" ]]; then
   echo -e '\nexport PATH=$HOME/go/bin:$PATH
  export GOROOT=/usr/local/go
  export GOPATH=$HOME/go
- export GO111MODULE=on
+ export GO111MODULE=auto
  export PATH=$PATH:$GOROOT/bin:$GOPATH/bin' >> $HOME/.bashrc;
   source $HOME/.bashrc
 else
